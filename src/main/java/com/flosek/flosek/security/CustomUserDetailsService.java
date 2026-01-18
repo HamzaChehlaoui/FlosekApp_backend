450e29d37db7796e88cdd441ba6d3e341de068fb
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2026-01-18 15:40:11 +0000

    feat(security): implement CustomUserDetailsService

---
+++ src/main/java/com/flosek/flosek/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.flosek.flosek.security;
+
+import com.flosek.flosek.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Custom UserDetailsService implementation for loading user from database
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmailAndDeletedAtIsNull(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
