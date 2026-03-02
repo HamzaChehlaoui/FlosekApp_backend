8e5c6b0204ad8cb5a5a540181dbb07fa0ee3ddf6
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2026-03-02 15:06:57 +0000

    feat(dto): add ContributionRequestDTO for savings goal contributions

---
+++ src/main/java/com/flosek/flosek/dto/request/ContributionRequestDTO.java
@@ -0,0 +1,26 @@
+package com.flosek.flosek.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * DTO for adding contribution to a savings goal
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContributionRequestDTO {
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
+    private BigDecimal amount;
+
+    private String note;
+}
